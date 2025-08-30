3737128be4a2eeae38b4e2c2b019023166835a20
Rashmi <rashmikvaidya11@gmail.com>
2025-08-31 00:16:04 +0530

    Second Commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import Home from "./pages/Home";
+import Result from "./pages/Result";
+import BuyCredit from "./pages/BuyCredit";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      {/* Add Routing */}
+      <Home />
+      <Result />
+      <BuyCredit />
+    </div>
+  );
 };
 
 export default App;
