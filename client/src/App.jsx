82ce34d2bbedb1b47730630c05a16d62c7fe99ea
Rashmi <rashmikvaidya11@gmail.com>
2025-09-03 13:13:27 +0530

    Login page features added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,13 +6,14 @@ import Result from "./pages/Result";
 import BuyCredit from "./pages/BuyCredit";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
+import Login from "./components/Login";
 
 const App = () => {
   return (
     <div className="px-4 sm:px-10 md:px-14 lg:px-28 min-h-screen  bg-gradient-to-b from-teal-50 to-orange-50 ">
       {/* Add Navigation */}
       <NavBar />
-
+      <Login />
       {/* Add Routing */}
       <Routes>
         <Route path="/" element={<Home />} /> {/*Home page route*/}
---
+++ client/src/components/Login.jsx
@@ -0,0 +1,81 @@
+import React from "react";
+import { assets } from "../assets/assets";
+import { useState } from "react";
+
+const Login = () => {
+  //State to manage switching between sign and sign up page
+
+  const [state, setState] = useState("Login");
+
+  return (
+    <div className="absolute top-0 left-0 right-0 bottom-0 backdrop-blur-lg z-10 bg-black/30 flex justify-center items-center">
+      <form className="relative bg-white p-10 rounded-2xl text-slate-500">
+        <h1 className="text-center text-2xl text-neutral-700 font-semibold">
+          {state}
+        </h1>
+        <p className="text-sm mt-3">
+          Welcome Back! Please Sign-in to Contiunue
+        </p>
+
+        {state !== "login" && (
+          <div className="border border-neutral-300 px-6  py-2 flex items-center gap-4 rounded-xl mt-4">
+            <img src={assets.profile_icon} width={20} alt="" />
+            <input
+              className="outline-none text-md"
+              type="text"
+              placeholder="Full Name"
+              id=""
+              required
+            />
+          </div>
+        )}
+
+        <div className="border border-neutral-300 px-6  py-2 flex items-center gap-4 rounded-xl mt-4">
+          <img src={assets.email_icon} alt="" />
+          <input
+            className="outline-none text-md"
+            type="email"
+            placeholder="Email Id"
+            id=""
+            required
+          />
+        </div>
+
+        <div className="border border-neutral-300 px-6 py-2 flex items-center gap-4 rounded-xl mt-4">
+          <img src={assets.lock_icon} alt="" />
+          <input
+            className="outline-none text-md"
+            type="password"
+            placeholder="Password"
+            id=""
+            required
+          />
+        </div>
+        <p className="text-sm text-blue-600 my-4 cursor-pointer">
+          Forgot password?
+        </p>
+        <button className="bg-gradient-to-l from-sky-600 to-blue-600  w-full text-white font-bold py-2 rounded-full ">
+          Create Account
+        </button>
+
+        <p className="mt-5 text-center">
+          Don't have an account?{" "}
+          <span className="text-blue-600 cursor-pointer">Sign Up</span>{" "}
+        </p>
+
+        <p className="mt-5 text-center">
+          Already have an account?{" "}
+          <span className="text-blue-600 cursor-pointer">Login</span>{" "}
+        </p>
+
+        <img
+          src={assets.cross_icon}
+          className="absolute top-5 right-5 cursor-pointer"
+          alt=""
+        />
+      </form>
+    </div>
+  );
+};
+
+export default Login;
